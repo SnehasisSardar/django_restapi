0dc0e91f6f7ff203d2d1e04eac618b3ce60bac28
SnehasisSardar <sardarsnehasis77@gmail.com>
2024-04-03 01:15:51 +0530

    Updated file

--- backend/apis/views.py
+++ backend/apis/views.py
@@ -1,10 +1,20 @@
 # from django.shortcuts import render
+import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 @csrf_exempt
 def api_home(request,*args,**kwargs):
+    print(request.GET)
     body=request.body #byte string of JSON data
-    print(body)
-    return JsonResponse({"message":"Hi there, This the Django API response you were looking for"})
+    data={}
+    try:
+        data=json.loads(body) #string of JSON data -> python dict
+    except:
+        pass
+    print(data)
+    print(request.headers)
+    data['headers']=dict(request.headers)
+    data['content_type']=request.content_type
+    return JsonResponse(data)
