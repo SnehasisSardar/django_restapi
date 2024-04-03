415e141870ae17a191f67f0527ea48fcae65f68e
SnehasisSardar <sardarsnehasis77@gmail.com>
2024-04-04 00:49:17 +0530

    Updated file

--- backend/apis/views.py
+++ backend/apis/views.py
@@ -2,19 +2,16 @@
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from product.models import Product
 
 # Create your views here.
 @csrf_exempt
 def api_home(request,*args,**kwargs):
-    print(request.GET)
-    body=request.body #byte string of JSON data
+    model_data=Product.objects.all().order_by("?").first()
     data={}
-    try:
-        data=json.loads(body) #string of JSON data -> python dict
-    except:
-        pass
-    print(data)
-    print(request.headers)
-    data['headers']=dict(request.headers)
-    data['content_type']=request.content_type
+    if model_data:
+        data['id']=model_data.id
+        data['title']=model_data.title
+        data['content']=model_data.content
+        data['price']=model_data.price
     return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,12 +4,12 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/apis/" #http://127.0.0.1:8000/ 
 
-get_response = requests.get(endpoint,params={"abc":123}, json={"querry": "Hello World" }) # HTTP Request
+get_response = requests.get(endpoint, json={"product_id":123 }) # HTTP Request
 # print(get_response.headers)
-print(get_response.text)
+# print(get_response.text)
 print(get_response.status_code)
 # HTTP Request -> HTML
 # REST API HTTP Request -> JSON
 # JavaScript Object Nototion ~ Python Dict
 # get_response = requests.get(endpoint)
-# print(get_response.json())
+print(get_response.json())
