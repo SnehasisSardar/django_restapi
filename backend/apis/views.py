1a44315d608c403aee870d45d40f78a319e5c4aa
SnehasisSardar <sardarsnehasis77@gmail.com>
2024-04-06 11:47:27 +0530

    Updated views.py

--- backend/apis/views.py
+++ backend/apis/views.py
@@ -1,6 +1,7 @@
 # from django.shortcuts import render
 import json
 from django.http import JsonResponse
+from django.forms.models import model_to_dict  # Used later in the code
 from django.views.decorators.csrf import csrf_exempt
 from product.models import Product
 
@@ -10,8 +11,10 @@ def api_home(request,*args,**kwargs):
     model_data=Product.objects.all().order_by("?").first()
     data={}
     if model_data:
-        data['id']=model_data.id
-        data['title']=model_data.title
-        data['content']=model_data.content
-        data['price']=model_data.price
+        # data['id']=model_data.id
+        # data['title']=model_data.title
+        # data['content']=model_data.content
+        # data['price']=model_data.price
+
+        data = model_to_dict(model_data) # Used for the same purpuse as the above 4 lines
     return JsonResponse(data)
