0efa0ca98ee753b3284ffae1fe3110b9c60beb8e
SnehasisSardar <sardarsnehasis77@gmail.com>
2024-04-07 15:36:56 +0530

    Updated file

--- backend/apis/views.py
+++ backend/apis/views.py
@@ -5,25 +5,20 @@ from django.http import JsonResponse
 from django.forms.models import model_to_dict  # Used later in the code
 from django.views.decorators.csrf import csrf_exempt
 
-# from rest_framework.response import Response
-# from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 from product.models import Product
 from product.serializers import ProductSerializer
 
 # Create your views here.
-# @api_view(["GET"])
+@api_view(["POST"])
 @csrf_exempt
 def api_home(request,*args,**kwargs):
-    instance=Product.objects.all().order_by("?").first()
-    data={}
-    if instance:  
-        # data['id']=model_data.id
-        # data['title']=model_data.title
-        # data['content']=model_data.content
-        # data['price']=model_data.price
-
-        # data = model_to_dict(instance,fields=['id','title','content','price','sale_price']) # Used for the same purpuse as the above 4 lines
-        data= ProductSerializer(instance).data
-    return JsonResponse(data)
-    # return Response(data)
+    serializer=ProductSerializer(data=request.data)
+    
+    if serializer.is_valid(raise_exception=True):
+        # instance=serializer.save()
+        print(serializer.data)
+    
+        return Response(serializer.data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,8 +4,8 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/apis/" #http://127.0.0.1:8000/ 
 
-get_response = requests.get(endpoint, json={"product_id":123 }) # HTTP Request
-# print(get_response.headers)
+get_response = requests.post(endpoint, json={"title":"Hello World" }) # HTTP Request
+# print(get_resp onse.headers)
 # print(get_response.text)
 print(get_response.status_code)
 # HTTP Request -> HTML
