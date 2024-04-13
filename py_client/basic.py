a49da0bf289623a82c60494c076f32c1c0051770
SnehasisSardar <sardarsnehasis77@gmail.com>
2024-04-14 01:18:39 +0530

    basic.py

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,7 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/apis/" #http://127.0.0.1:8000/ 
 
-get_response = requests.post(endpoint, json={"title":"Hello World" }) # HTTP Request
+get_response = requests.post(endpoint, json={"title":"Hello World",'content':"World is Cruel", 'price':58 }) # HTTP Request
 # print(get_resp onse.headers)
 # print(get_response.text)
 print(get_response.status_code)
