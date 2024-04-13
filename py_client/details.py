a256d8f9d18c1781691c8fd8fda075c49678cba9
SnehasisSardar <sardarsnehasis77@gmail.com>
2024-04-14 01:20:33 +0530

    Initial commit

---
+++ py_client/details.py
@@ -0,0 +1,10 @@
+import requests
+
+endpoint = "http://localhost:8000/apis/product/2/" #http://127.0.0.1:8000/ 
+
+get_response = requests.get(endpoint) # HTTP Request
+
+# print(get_response.text)
+print(get_response.status_code)
+
+print(get_response.json())
