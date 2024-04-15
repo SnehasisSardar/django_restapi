8a4d50190912fbac982ff43a9b70af6e4cba3386
SnehasisSardar <sardarsnehasis77@gmail.com>
2024-04-16 02:25:46 +0530

    Added Retrieve API

--- backend/product/views.py
+++ backend/product/views.py
@@ -13,3 +13,24 @@ class ProductCreateAPIView(generics.CreateAPIView):
     serializer_class = ProductSerializer
     # lookup_field = 'pk'
     
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        # print(serializer.validated_data)
+        title = serializer.validated_data.get('title')
+        content=serializer.validated_data.get('content') or None 
+        if content is None:
+            content = title
+        
+        serializer.save(content=content)
+
+class ProductListCreateAPIView(generics.ListCreateAPIView):
+    queryset= Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self,serializer):
+        title = serializer.validated_data.get('title')
+        content = serializer.validate_get('content')or None
+        if content is None:
+            content = title
+
+            serializer.save(content=content)
