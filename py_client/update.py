04890a2b32e8fdff449462b5307345c9fff67551
SnehasisSardar <sardarsnehasis77@gmail.com>
2024-04-22 01:06:15 +0530

    Updated file

--- py_client/update.py
+++ py_client/update.py
@@ -1,10 +1,10 @@
 import requests
 
-endpoint = "http://localhost:8000/apis/product/1/update/" #http://127.0.0.1:8000/ 
+endpoint = "http://localhost:8000/apis/product/11/update/" #http://127.0.0.1:8000/ 
 
 data = {
-    'title': 'Hello sir . How are You',
-    'price': 120
+    'title': 'Playing free fire',
+    'price': 100
 }
 
 get_response = requests.put(endpoint, json=data) # HTTP Request
