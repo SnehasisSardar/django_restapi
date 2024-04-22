09655fa7074bd78d3fc791de40248f3e18d93764
SnehasisSardar <sardarsnehasis77@gmail.com>
2024-04-23 01:34:34 +0530

    Updated urls

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -3,9 +3,9 @@ from  django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/',views.ProductMixinView.as_view()),
-    path('<int:pk>/update/',views.ProductMixinView.as_view()),
-    path('<int:pk>/delete/',views.ProductMixinView.as_view()),
-    path('',views.ProductMixinView.as_view())
+    path('<int:pk>/',views.ProductDetailsAPIView.as_view()),
+    path('<int:pk>/update/',views.ProductUpdateAPIView.as_view()),
+    path('<int:pk>/delete/',views.ProductDeleteAPIView.as_view()),
+    path('',views.ProductListCreateAPIView.as_view())
 
 ]
