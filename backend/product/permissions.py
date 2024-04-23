cc77f947aa1feb21c2006dd81463a8adf56119de
SnehasisSardar <sardarsnehasis77@gmail.com>
2024-04-24 02:24:08 +0530

    Initial commit

---
+++ backend/product/permissions.py
@@ -0,0 +1,25 @@
+from rest_framework import permissions
+
+class IsStaffEditorPermission(permissions.DjangoModelPermissions):
+    def has_permission(self, request, view):
+        # if not request.user.username== "admin":
+        #     return False
+        if not request.user.is_staff:
+            return False
+        return super().has_permission(request, view)
+
+
+    # def has_permission(self, request, view):
+    #     user=request.user
+    #     print(user.get_all_permissions())
+    #     if user.is_staff:
+    #         if user.has_perm("product.view_product"):
+    #             return True
+    #         if user.has_perm("product.add_product"):
+    #             return True
+    #         if user.has_perm("product.change_product"):
+    #             return True
+    #         if user.has_perm("product.delete_product"):
+    #             return True
+    #         return False
+    #     return False
