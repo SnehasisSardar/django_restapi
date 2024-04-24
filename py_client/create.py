f77f9ad66889360b4dfe144da52a9e0a3bb9c74b
SnehasisSardar <sardarsnehasis77@gmail.com>
2024-04-25 01:29:59 +0530

    Added authtokens

--- py_client/create.py
+++ py_client/create.py
@@ -1,8 +1,10 @@
 import requests
 
+headers ={'Authorization': 'Bearer 773580ea2677405cbebba95fb61fee22ca1f07b4'}
+
 endpoint = "http://localhost:8000/apis/product/" #http://127.0.0.1:8000/ 
 
-get_response = requests.post(endpoint,json={'title':"This is title",'price':50}) # HTTP Request
+get_response = requests.post(endpoint,json={'title':"This is title with tokens",'price':500},headers=headers) # HTTP Request
 
 # print(get_response.text)
 print(get_response.status_code)
