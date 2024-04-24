bfedd483bd83cde4a86e70ee2b76b30353d5f7f0
SnehasisSardar <sardarsnehasis77@gmail.com>
2024-04-25 01:30:45 +0530

    New authentication classes are added

--- backend/product/views.py
+++ backend/product/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
 
+from apis.authentication import TokenAuthentication
+
 from .models import Product
 from .serializers import ProductSerializer
 from .permissions import IsStaffEditorPermission
@@ -15,7 +17,7 @@ class ProductDetailsAPIView(generics.RetrieveAPIView):
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset= Product.objects.all()
     serializer_class = ProductSerializer
-    authentication_classes = [authentication.SessionAuthentication]
+    authentication_classes = [authentication.SessionAuthentication,TokenAuthentication]
     permission_classes= [permissions.IsAdminUser, IsStaffEditorPermission]
 
     def perform_create(self,serializer):
