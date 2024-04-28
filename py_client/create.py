fc15713d78c786cbbcde09b033b7797cb3d0641d
SnehasisSardar <sardarsnehasis77@gmail.com>
2024-04-28 13:46:52 +0530

    Updated file

--- py_client/create.py
+++ py_client/create.py
@@ -1,10 +1,10 @@
 import requests
 
-headers ={'Authorization': 'Bearer 773580ea2677405cbebba95fb61fee22ca1f07b4'}
+# headers ={'Authorization': 'Bearer c71ba4d05a1f9cd9aa88146d2ac729361b6ef378'}
 
 endpoint = "http://localhost:8000/apis/product/" #http://127.0.0.1:8000/ 
 
-get_response = requests.post(endpoint,json={'title':"This is title with tokens",'price':500},headers=headers) # HTTP Request
+get_response = requests.post(endpoint,json={'title':"This is title with tokens",'price':500})#,headers=headers) # HTTP Request
 
 # print(get_response.text)
 print(get_response.status_code)
