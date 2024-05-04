b6bac54982a902fa2c6cb8cd82579adaf0e2f729
SnehasisSardar <sardarsnehasis77@gmail.com>
2024-05-05 01:42:39 +0530

    links added

--- backend/product/serializers.py
+++ backend/product/serializers.py
@@ -3,7 +3,7 @@ from rest_framework.reverse import reverse
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
-    edit_url = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.HyperlinkedIdentityField(view_name="product-edit",lookup_field="pk")
     url=serializers.HyperlinkedIdentityField(view_name="product-detail",lookup_field="pk")
     class Meta:
         model = Product
@@ -17,9 +17,9 @@ class ProductSerializer(serializers.ModelSerializer):
             'sale_price'
         ]        
 
-    def get_edit_url(self,obj):
-        # return f"/apis/v2/product/{obj.pk}/"
-        request= self.context.get('request')
-        if request is None:
-            return None
-        return reverse("product-edit",kwargs={"pk":obj.pk}, request=request)
+    # def get_edit_url(self,obj):
+    #     # return f"/apis/v2/product/{obj.pk}/"
+    #     request= self.context.get('request')
+    #     if request is None:
+    #         return None
+    #     return reverse("product-edit",kwargs={"pk":obj.pk}, request=request)
